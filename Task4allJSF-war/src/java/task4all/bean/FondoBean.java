33471033785e775e11818008ff4b313077011135
Julio <julioog.94@gmail.com>
2016-06-26 04:42:43 +0200

    Selector de fondo en editar proyecto añadido

---
+++ Task4allJSF-war/src/java/task4all/bean/FondoBean.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package task4all.bean;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class FondoBean {
+    
+    private List<String> images;
+
+    public FondoBean() {
+    }   
+     
+    @PostConstruct
+    public void init() {
+        images = new ArrayList<>();
+        for (int i = 0; i <= 11; i++) {
+            images.add("texture" + i + ".png");
+        }
+    }
+ 
+    public List<String> getImages() {
+        return images;
+    }
+}
